383463dafe88b06dd2cfcd0c227afe24cf067476
davidhalldor <davidhalldor@a5d971b9-48d3-f69d-0da5-7c4037925faf>
2012-03-16 13:55:59 +0000

    Buy og sell og komið.

--- trunk/public/javascripts/rubroker.js
+++ trunk/public/javascripts/rubroker.js
@@ -4,6 +4,7 @@
         this.price = ko.observable(data.price);
         this.numowned = ko.observable(data.numowned);
         this.isOwned = ko.observable(data.isOwned);
+        this.total = ko.observable(data.total);
     }
 
     function StockListViewModel() {
@@ -11,26 +12,57 @@
         var self = this;
         self.stocks = ko.observableArray([]);
         self.stocksOwned = ko.observableArray([]);
-        self.credit = 1000;
+        self.credit = ko.observable(1000);
         self.numBought = ko.observable();
+        self.numSell = ko.observable();
+        self.newcredit = ko.computed(function() {
+            ko.utils.arrayForEach(self.stocksOwned(), function(item){
+                item.total(item.numowned() * item.price());
+            });
+        });
 
         // Operations
         self.buyStock = function (stock) {
-            //console.log('num ' + self.numowned());
-
-            var result = self.stocksOwned.indexOf(stock);
-            if (result > -1){
-                var prevStock = self.stocksOwned()[result];
-                //prevStock.numowned(self.numBought + prevStock.numBought)
-                //stock.numowned(stock + self.numowned);
-            } else {
-                //stock.numowned(self.numowned());
-                self.stocksOwned.push(stock);
+            console.log('num ' + self.numBought());
+            var prevNumowned = parseInt(stock.numowned());
+            var numBought = parseInt(self.numBought());
+            var oldPrice = parseInt(stock.price());
+            var newCredit = self.credit() - (numBought * oldPrice);
+            var newPrice = oldPrice * ( 1 + (Math.pow(numBought,2)/100));
+            if (self.credit() >= newPrice) {
+                stock.price(newPrice);
+                self.credit(newCredit);
+                stock.numowned(prevNumowned + numBought);
+                var result = self.stocksOwned.indexOf(stock);
+                if (result == -1){
+                    self.stocksOwned.push(stock);
+                }
+            }
+            else {
+                alert("You don't have enough credit!");
             }
             self.numBought("");
         };
         self.sellStock = function (stock) {
-            self.stocksOwned.remove(stock)
+            console.log('num ' + self.numBought());
+            var prevNumowned = parseInt(stock.numowned());
+            var numSell = parseInt(self.numSell());
+            var oldPrice = parseInt(stock.price());
+            var newCredit = self.credit() + (numSell * oldPrice);
+            var newPrice = oldPrice / ( 1 + (Math.pow(numSell,2)/100));
+            if (prevNumowned >= numSell) {
+                stock.price(newPrice);
+                self.credit(newCredit);
+                stock.numowned(prevNumowned - numSell);
+                var result = self.stocksOwned.indexOf(stock);
+                if (stock.numowned() <= 0 ){
+                    self.stocksOwned.remove(stock);
+                }
+            }
+            else {
+                alert("You don't have enough stocks to sell!");
+            }
+            self.numSell("");
         };
 
         // Load initial state from server, convert it to Stock instances, then populate self.stocks
